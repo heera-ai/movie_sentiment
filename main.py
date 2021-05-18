0d386c8191bde980aeac43cdc30e14ddd670ad9d
HEERA LAL <54330840+heerendrachoudhary@users.noreply.github.com>
2021-05-18 10:32:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-from sklearn.externals import joblib
+import joblib
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
