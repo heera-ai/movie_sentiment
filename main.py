9aeb86728f0635ccf34cbf80bac318aa3bed9819
HEERA LAL <54330840+heerendrachoudhary@users.noreply.github.com>
2021-05-18 10:58:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from PIL import Image
 import pickle
 import nltk
+nltk.download()
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 stop_words = set(stopwords.words('english'))
